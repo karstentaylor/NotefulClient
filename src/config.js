48d73688e2085c6c4407d5aad6c9c6896f29f3e6
Karsten Taylor <karstentaylor0@gmail.com>
2020-09-07 15:26:16 -0700

    removed /

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-	API_ENDPOINT: 'https://noteful-assignment.herokuapp.com/',
+	API_ENDPOINT: 'https://noteful-assignment.herokuapp.com',
 	// API_ENDPOINT: 'http://localhost:8000',
 };
