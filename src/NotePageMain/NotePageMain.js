9facc6354303f996308c6f3eea3a9140a306eb05
Karsten Taylor <karstentaylor0@gmail.com>
2020-09-12 20:48:28 -0700

    delete fixed ready for deploy

--- src/NotePageMain/NotePageMain.js
+++ src/NotePageMain/NotePageMain.js
@@ -1,44 +1,45 @@
-import React from 'react'
-import Note from '../Note/Note'
-import ApiContext from '../ApiContext'
-import { findNote } from '../notes-helpers'
-import './NotePageMain.css'
+import React from 'react';
+import Note from '../Note/Note';
+import ApiContext from '../ApiContext';
+import { findNote } from '../notes-helpers';
+import './NotePageMain.css';
 
 export default class NotePageMain extends React.Component {
-  static defaultProps = {
-    match: {
-      params: {}
-    }
-  }
-  static contextType = ApiContext
+	static defaultProps = {
+		match: {
+			params: {},
+		},
+	};
+	static contextType = ApiContext;
 
-  handleDeleteNote = noteId => {
-    this.props.history.push(`/`)
-  }
+	handleDeleteNote = (noteId) => {
+		console.log('working');
+		this.props.history.push(`/`);
+	};
 
-  render() {
-    const { notes=[] } = this.context
-    const { noteId } = this.props.match.params
-    const note = findNote(notes, noteId) || { note_content: '' }
-    console.log(findNote(notes, noteId));
-    console.log(notes);
-    console.log(noteId);
-    console.log(this.context.notes)
-    console.log(this.context.folders)
-    return (
-      <section className='NotePageMain'>
-        <Note
-          id={note.id}
-          name={note.note_name}
-          modified={note.modified}
-          onDeleteNote={this.handleDeleteNote}
-        />
-        <div className='NotePageMain__content'>
-          {note.note_content.split(/\n \r|\n/).map((para, i) =>
-            <p key={i}>{para}</p>
-          )}
-        </div>
-      </section>
-    )
-  }
+	render() {
+		const { notes = [] } = this.context;
+		const { noteId } = this.props.match.params;
+		const note = findNote(notes, noteId) || { note_content: '' };
+		console.log(findNote(notes, noteId));
+		console.log(notes);
+		console.log(noteId);
+		console.log(this.context.notes);
+		console.log(this.context.folders);
+		return (
+			<section className="NotePageMain">
+				<Note
+					id={note.id}
+					name={note.note_name}
+					modified={note.modified}
+					onDeleteNote={this.handleDeleteNote}
+				/>
+				<div className="NotePageMain__content">
+					{note.note_content.split(/\n \r|\n/).map((para, i) => (
+						<p key={i}>{para}</p>
+					))}
+				</div>
+			</section>
+		);
+	}
 }
