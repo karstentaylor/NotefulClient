ccaf50cb22aa35de995d7756526db62e645505ca
Karsten Taylor <karstentaylor0@gmail.com>
2020-09-12 18:26:09 -0700

    fixed getNotesForFolder

--- src/notes-helpers.js
+++ src/notes-helpers.js
@@ -1,15 +1,11 @@
+export const findFolder = (folders = [], folderId) =>
+	folders.find((folder) => folder.id === folderId);
 
-export const findFolder = (folders=[], folderId) =>
-  folders.find(folder => folder.id === folderId)
+export const findNote = (notes = [], noteId) =>
+	notes.find((note) => note.id === noteId);
 
-export const findNote = (notes=[], noteId) =>
-  notes.find(note => note.id === noteId)
+export const getNotesForFolder = (notes = [], folderId) =>
+	notes.filter((e) => e.folder_name === parseInt(folderId));
 
-export const getNotesForFolder = (notes=[], folderId) => (
-  (!folderId)
-    ? notes
-    : notes.filter(note => note.folder_name === folderId)
-)
-
-export const countNotesForFolder = (notes=[], folderId) =>
-  notes.filter(note => note.folder_name === folderId).length
+export const countNotesForFolder = (notes = [], folderId) =>
+	notes.filter((note) => note.folder_name === folderId).length;
